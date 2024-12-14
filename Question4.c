5b403cd459869f65cf40932e7907330a7fe8c2cd
lukacassar <luka.cassar.24@um.edu.mt>
2024-12-14 17:24:43 +0100

    hi

---
+++ Question4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+void dectohex(int num) {
+
+    if (num == 0) {
+        return;
+    }
+
+    int rem = num % 16;
+    dectohex(num / 16);
+
+    if (rem < 10) {
+        printf("%d", rem);
+    } else {
+        printf("%c", 'A' + rem - 10);
+    }
+}
+
+int main() {
+    int num = 40;
+    dectohex(num);
+}
