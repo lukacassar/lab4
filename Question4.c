def6748293c0519502b449576ee7d0f448a6cc85
lukacassar <luka.cassar.24@um.edu.mt>
2024-12-15 14:53:02 +0100

    recursionnnn

--- Question4.c
+++ Question4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 void dectohex(int num) {
@@ -17,7 +18,24 @@ void dectohex(int num) {
     }
 }
 
+void rev(char text[], int n) {
+    int len = strlen(text);
+      if (n >= len/2) {
+        return;
+    }
+
+
+    char temp = text[n];
+    text[n] = text[len - 1 - n];
+    text[len - 1 - n] = temp;
+    rev(text, n+1);
+}
+
 int main() {
     int num = 40;
     dectohex(num);
+
+    char text[40] = "woof meow 123";
+    rev(text, 0);
+    printf("\nThe reversed string: %s", text);
 }
